bdffaa1d6a5b2507b198f520aa7e79bc122d4edf
analyst15 <alexokeyo15@gmail.com>
2026-02-16 22:54:32 +0300

    addec navbar & footer to layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <div className="absolute top-0 w-full z-50 px-4 pt-4 sm:pt-6">
+          <Navbar />
+        </div>
+        <Footer />
       </body>
     </html>
   );
